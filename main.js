f62ab71b48465a3debcb9e6fd008c7892978eded
Aditya Sharma <ads.as2020@gmail.com>
2026-01-11 18:25:18 +0530

    made the barebones ui

--- main.js
+++ main.js
@@ -1,3 +1,40 @@
 import { knightMoves } from "./engine/knight.js";
 
-console.log(knightMoves([0, 0], [7, 7]));
+const board = document.getElementById("board");
+
+let start = null;
+let end = null;
+
+for (let y = 7; y >= 0; y--) {
+	for (let x = 0; x < 8; x++) {
+		const tile = document.createElement("div");
+		tile.className = "tile";
+		tile.dataset.pos = [x, y];
+
+		tile.onclick = () => handleClick(x, y, tile);
+		board.appendChild(tile);
+	}
+}
+
+function handleClick(x, y, tile) {
+	if (!start) {
+		start = [x, y];
+		tile.classList.add("start");
+		return;
+	}
+
+	if (!end) {
+		end = [x, y];
+		tile.classList.add("end");
+		drawPath();
+	}
+}
+
+function drawPath() {
+	const path = knightMoves(start, end);
+
+	path.forEach(([px, py]) => {
+		const index = (7 - py) * 8 + px;
+		board.children[index].classList.add("path");
+	});
+}
